4b5ad9896d787f25344356144fd5be894780f7e3
sushant2040 <sushantnikam2040@gmail.com>
2025-08-07 09:09:30 +0530

    add files

--- backend/src/main/java/com/language/detector/App.java
+++ backend/src/main/java/com/language/detector/App.java
@@ -48,7 +48,7 @@ public class App extends SpringBootServletInitializer
 		}
 		@Bean
 	    public TranscribeClient transcribeClient() {
-			AwsCredentials awsCredentials = AwsBasicCredentials.create(environment.getProperty("AWS_S3_ACCESS_KEY"),environment.getProperty("AWS_S3_SECRET_ACCESS_KEY"));
+			AwsCredentials awsCredentials = AwsBasicCredentials.create(environment.getProperty("vocalKord.aws.accessKey"),environment.getProperty("vocalKord.aws.secretAccessKey"));
 	        return TranscribeClient.builder()
 	        		.credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
 	                .region(Region.US_WEST_2) // Change this to your AWS region
@@ -57,7 +57,7 @@ public class App extends SpringBootServletInitializer
 
 	    @Bean
 	    public S3Client s3Client() {
-	    	AwsCredentials awsCredentials = AwsBasicCredentials.create(environment.getProperty("AWS_S3_ACCESS_KEY"),environment.getProperty("AWS_S3_SECRET_ACCESS_KEY"));
+	    	AwsCredentials awsCredentials = AwsBasicCredentials.create(environment.getProperty("vocalKord.aws.accessKey"),environment.getProperty("vocalKord.aws.secretAccessKey"));
 	        return S3Client.builder()
 	        		.credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
 	                .region(Region.US_WEST_2) // Same region as your S3 bucket
